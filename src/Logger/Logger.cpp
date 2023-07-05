2f50988d3c85ca36297306dfd91ffc4d138f9529
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-07-05 23:19:01 +0530

    fix: Replace \n with endl to flush the result properly

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -2,10 +2,12 @@
 
 #include <iostream>
 
-void Logger::log(const std::string &message) { std::cout << message << "\n"; }
+void Logger::log(const std::string &message) {
+    std::cout << message << std::endl;
+}
 
 void Logger::log(const std::string &message, const std::string &filename) {
-    std::cout << message << " : " << filename << "\n";
+    std::cout << message << " : " << filename << std::endl;
 }
 
 // TODO: output file
