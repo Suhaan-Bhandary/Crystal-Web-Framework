861940d126b40bcc60a6e54983ae9f768177c85e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-09-13 00:19:17 +0530

    feat(Logger): Updated the logger method to take variable argument
    Now the logger macros can take any number of arguments with any types
    which are defined for cout.

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -2,38 +2,8 @@
 
 #include <iostream>
 
-const std::string blue("\033[0;34m");
-const std::string yellow("\033[0;33m");
-const std::string red("\033[0;31m");
-const std::string reset("\033[0m");
-
 void Logger::log(const std::string& message) {
     std::cout << message << std::endl;
-}
-
-void Logger::log(const std::string& message, const char* filename, int line) {
-    std::cout << "[" << filename << ":" << line << "] " << message << std::endl;
-}
-
-void Logger::logNote(const std::string& message, const char* filename,
-                     int line) {
-    std::cout << blue;
-    std::cout << "[" << filename << ":" << line << "] " << message;
-    std::cout << reset << std::endl;
-}
-
-void Logger::logWarn(const std::string& message, const char* filename,
-                     int line) {
-    std::cout << yellow;
-    std::cout << "[" << filename << ":" << line << "] " << message;
-    std::cout << reset << std::endl;
-}
-
-void Logger::logError(const std::string& message, const char* filename,
-                      int line) {
-    std::cout << red;
-    std::cout << "[" << filename << ":" << line << "] " << message;
-    std::cout << reset << std::endl;
 }
 
 // TODO: output file
--- src/Logger/Logger.h
+++ src/Logger/Logger.h
@@ -2,6 +2,7 @@
 #ifndef INCLUDED_LOGGER
 #define INCLUDED_LOGGER
 
+#include <iostream>
 #include <string>
 class Logger {
    private:
@@ -10,30 +11,69 @@ class Logger {
    public:
     static void log(const std::string& message);
 
-    static void log(const std::string& message, const char* filename, int line);
+    template <typename... Args>
+    static void log(const char* filename, int line, Args... args);
 
-    static void logNote(const std::string& message, const char* filename,
-                        int line);
+    template <typename... Args>
+    static void logNote(const char* filename, int line, Args... args);
 
-    static void logWarn(const std::string& message, const char* filename,
-                        int line);
+    template <typename... Args>
+    static void logWarn(const char* filename, int line, Args... args);
 
-    static void logError(const std::string& message, const char* filename,
-                         int line);
+    template <typename... Args>
+    static void logError(const char* filename, int line, Args... args);
 };
 
+// C++ Variadic Template has to be defined inside the .h file itself
+// https://stackoverflow.com/questions/25091516/c-template-variadic-function-undefined-reference
+const std::string blue("\033[0;34m");
+const std::string yellow("\033[0;33m");
+const std::string red("\033[0;31m");
+const std::string reset("\033[0m");
+
+template <typename... Args>
+void Logger::log(const char* filename, int line, Args... args) {
+    std::cout << "[" << filename << ":" << line << "] ";
+    ((std::cout << args << " "), ...);
+    std::cout << std::endl;
+}
+
+template <typename... Args>
+void Logger::logNote(const char* filename, int line, Args... args) {
+    std::cout << blue;
+    std::cout << "[" << filename << ":" << line << "] ";
+    ((std::cout << args << " "), ...);
+    std::cout << reset << std::endl;
+}
+
+template <typename... Args>
+void Logger::logWarn(const char* filename, int line, Args... args) {
+    std::cout << yellow;
+    std::cout << "[" << filename << ":" << line << "] ";
+    ((std::cout << args << " "), ...);
+    std::cout << reset << std::endl;
+}
+
+template <typename... Args>
+void Logger::logError(const char* filename, int line, Args... args) {
+    std::cout << red;
+    std::cout << "[" << filename << ":" << line << "] ";
+    ((std::cout << args << " "), ...);
+    std::cout << reset << std::endl;
+}
+
 #ifdef DEVELOPMENT_ENVIRONMENT
-#define LOGGER(message) Logger::log(message, __FILE__, __LINE__)
 #define LOGGER_MINIMAL(message) Logger::log(message)
-#define LOGGER_NOTE(message) Logger::logNote(message, __FILE__, __LINE__)
-#define LOGGER_WARNING(message) Logger::logWarn(message, __FILE__, __LINE__)
-#define LOGGER_ERROR(message) Logger::logError(message, __FILE__, __LINE__)
+#define LOGGER(...) Logger::log(__FILE__, __LINE__, __VA_ARGS__)
+#define LOGGER_NOTE(...) Logger::logNote(__FILE__, __LINE__, __VA_ARGS__)
+#define LOGGER_WARNING(...) Logger::logWarn(__FILE__, __LINE__, __VA_ARGS__)
+#define LOGGER_ERROR(...) Logger::logError(__FILE__, __LINE__, __VA_ARGS__)
 #else
-#define LOGGER(message)
 #define LOGGER_MINIMAL(message)
-#define LOGGER_NOTE(message)
-#define LOGGER_WARNING(message)
-#define LOGGER_ERROR(message) Logger::logError(message, __FILE__, __LINE__)
+#define LOGGER(...)
+#define LOGGER_NOTE(...)
+#define LOGGER_WARNING(...)
+#define LOGGER_ERROR(...) Logger::logError(__FILE__, __LINE__, __VA_ARGS__)
 #endif
 
 #endif
