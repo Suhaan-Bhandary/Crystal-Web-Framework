b7e584b136a288bef632f7734a80cf55dcb7f616
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-10-29 15:16:50 +0530

    feat: Changed the design of home page to have more information about the server

--- app/public/home/script.js
+++ app/public/home/script.js
@@ -1,16 +1,29 @@
-const popUpButton = document.getElementById('popUpButton');
-const closeButton = document.getElementById('closeButton');
-const popUpWindow = document.getElementById('popUpWindow');
+// Fetch Feature Items
+const featureItems = document.querySelectorAll(".features__list > button");
+const featureContents = document.querySelectorAll(".features__content > div");
 
-popUpButton.addEventListener('click', () => {
-  popUpWindow.classList.toggle('active');
-});
+const handleFeatureClick = (event) => {
+  let buttonId = "";
 
-closeButton.addEventListener('click', () => {
-  popUpWindow.classList.toggle('active');
-});
+  if (event.target.tagName.toLowerCase() === "li") {
+    buttonId = event.target.parentElement.id;
+  } else {
+    buttonId = event.target.id;
+  }
 
-popUpWindow.addEventListener('click', (event) => {
-  if (event.target.id !== 'popUpWindow') return;
-  popUpWindow.classList.toggle('active');
-});
+  // reset all
+  featureItems.forEach((item) => item.classList.remove("active"));
+  featureContents.forEach((item) => item.classList.remove("active"));
+
+  // Add active to the target and the content div
+  const content = document.getElementById(`${buttonId}-content`);
+  const button = document.getElementById(buttonId);
+
+  button.classList.add("active");
+  content.classList.add("active");
+};
+
+// Features Page
+featureItems.forEach((item) =>
+  item.addEventListener("click", handleFeatureClick)
+);
